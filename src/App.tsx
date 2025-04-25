80a66625188b1df059db10864c384bfb9509b1a1
WillianM19 <willianm1928@gmail.com>
2025-04-25 14:17:55 -0300

    feat: base de componente de taskcard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Button from "./components/Button"
 import Input from "./components/Input"
+import TaskCard from "./components/TaskCard"
 import Textbox from "./components/Textbox"
 
 
@@ -9,11 +10,16 @@ function App() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-3xl font-bold">Lista de Tarefas</h1>
 
-      <div className="border border-gray-800/30 rounded-md mt-3 w-[60vw] min-h-[40vh] p-4 flex flex-col gap-4">
+      <div className="border border-gray-800/30 rounded-md mt-3 w-[60vw] min-h-[40vh] p-4 flex flex-col gap-8">
         <h1 className="text-xl font-bold">Nova tarefa</h1>
         <Input placeholder="Titulo da tarefa" />
         <Textbox placeholder="Descrição (opcional)" />
         <Button>Adicionar Tarefa</Button>
+      </div>
+
+      <div className="w-[60vw] mt-3 flex gap-4">
+        <TaskCard />
+        <TaskCard />
       </div>
     </div>
   )
---
+++ src/components/TaskCard/index.tsx
@@ -0,0 +1,8 @@
+export default function TaskCard() {
+    return (
+        <div className="border border-gray-800/30 rounded-md p-4 flex flex-col gap-4 flex-1">
+            <h1 className="text-md font-bold">Titulo da tarefa</h1>
+            <p>Descrição da tarefa</p>
+        </div>
+    )
+}
