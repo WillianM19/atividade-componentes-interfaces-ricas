d14e3c354e4b16de7e761437d60b15de754eb576
WillianM19 <willianm1928@gmail.com>
2025-04-25 22:09:10 -0300

    feat: adiciona componente de chekbox

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+export default function Checkbox(props: {
+  checked?: boolean;
+  onChange?: (checked: boolean) => void;
+}) {
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={props.checked}
+        onChange={(e) => props.onChange?.(e.target.checked)}
+        className="w-4 h-4 rounded-md border border-gray-800/30"
+      />
+    </div>
+  )
+}
--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,7 +1,12 @@
+import Checkbox from "../Checkbox";
+
 export default function TaskCard() {
     return (
         <div className="border border-gray-800/30 rounded-md p-4 flex flex-col gap-4 flex-1">
-            <h1 className="text-md font-bold">Titulo da tarefa</h1>
+            <div className="flex gap-2 items-center">
+                <Checkbox />
+                <h1 className="text-md font-bold">Titulo da tarefa</h1>
+            </div>
             <p>Descrição da tarefa</p>
         </div>
     )
